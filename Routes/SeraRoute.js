979c894f7caeaa66a0464ed96d3d06d1800af120
Rash <herindrainyall2003@gmail.com>
2024-05-29 12:52:57 +0300

    update cors

--- Routes/SeraRoute.js
+++ Routes/SeraRoute.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const SeraController = require('../controllers/SeraController') 
-const { diskStorage } = require('multer')
 const multer = require('multer')
 
 
