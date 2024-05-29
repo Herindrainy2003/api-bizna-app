abfd853cddd04b57d6d354b2489b9c3a8eb7d90e
Rash <herindrainyall2003@gmail.com>
2024-05-29 12:50:41 +0300

    update cors

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const ConnectDb = require('./config/dbconnect')
 const SeraRoute = require('./Routes/SeraRoute')
 const routerTadySera = require('./Routes/SeraTadiavinaRoutes')
 const dotenv = require('dotenv')
-
+const cors = require('cors');
 
 dotenv.config()
 ConnectDb()
  
+// Autoriser uniquement les requêtes provenant de l'origine de votre application React
+const corsOptions = {
+    origin: 'https://seramalaky.onrender.com'
+  };
+  
+  // Activer CORS avec les options spécifiées
+  app.use(cors(corsOptions));
+  
+
 app.use('/api/sera/' ,SeraRoute)
 app.use('/api/hitadySera/' , routerTadySera)
 
-const PORT = process.env.PORT
+
+const PORT = process.env.PORT ||1000
 app.listen(PORT ,()=>console.log(`Rendez-vous sur localhost:${PORT}`))
